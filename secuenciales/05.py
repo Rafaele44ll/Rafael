d4b25023792a0e6f1e52abbd7014c44ad67e0f4c
Rafael <davidrafaelezxc@gmail.com>
2024-10-13 18:19:24 -0500

    py

--- secuenciales/05.py
+++ secuenciales/05.py
@@ -1,5 +1,14 @@
 import os
 os.system("cls")
 
+capacidad_gb = int(input("Ingrese la capacidad del disco duro en gigabytes: "))
+
+capacidad_mb = capacidad_gb * 1024                
+capacidad_kb = capacidad_mb * 1024                 
+capacidad_bytes = capacidad_kb * 1024             
+
+print(f"Capacidad en megabytes: {capacidad_mb} MB")
+print(f"Capacidad en kilobytes: {capacidad_kb} KB")
+print(f"Capacidad en bytes: {capacidad_bytes} bytes")
 
 
