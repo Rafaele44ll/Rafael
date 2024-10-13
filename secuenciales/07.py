6dd3065a2e081ab58bd845fef5718a124f7ea9f1
Rafael <davidrafaelezxc@gmail.com>
2024-10-13 18:20:20 -0500

    py

--- secuenciales/07.py
+++ secuenciales/07.py
@@ -1,3 +1,11 @@
 import os
 os.system("cls")
 
+base = int(input("Ingrese la base del rectángulo: "))
+altura = int(input("Ingrese la altura del rectángulo: "))
+
+area = base * altura                  
+perimetro = 2 * (base + altura)      
+
+print(f"Área del rectángulo: {area}")
+print(f"Perímetro del rectángulo: {perimetro}")
