47ffe3b2bbb9925954830522684f4467878e75a4
Rafael <davidrafaelezxc@gmail.com>
2024-10-12 22:17:21 -0500

    py

--- condicionales/08.py
+++ condicionales/08.py
@@ -1,2 +1,15 @@
 import os
 os.system("cls")
+
+propina_base = 20
+
+examenes_aprobados = int(input("Ingrese la cantidad de exámenes aprobados (0-3): "))
+
+if 0 <= examenes_aprobados <= 3:
+    propina_adicional = examenes_aprobados * 5
+
+    total_propina = propina_base + propina_adicional
+
+    print(f"El monto total de la propina es: S/. {total_propina}")
+else:
+    print("La cantidad de exámenes aprobados debe estar entre 0 y 3.")
