e6a31a2a57aac4d38aace5a8bb0d6c78d9827ad1
Rafael <davidrafaelezxc@gmail.com>
2024-10-13 16:16:55 -0500

    py

--- condicionales/27.py
+++ condicionales/27.py
@@ -1,2 +1,27 @@
 import os
 os.system("cls")
+
+monto_vendido = int(input("Ingrese el monto total vendido: "))
+
+sueldo_basico = 600
+
+comision = (15 * monto_vendido) // 100
+sueldo_bruto = sueldo_basico + comision
+
+# Calcular el descuento
+if sueldo_bruto > 1800:
+    descuento = (10 * sueldo_bruto) // 100  
+else:
+    descuento = (5 * sueldo_bruto) // 100   
+
+sueldo_neto = sueldo_bruto - descuento
+
+if monto_vendido > 1000:
+    polos_obsequiados = 3  
+
+    polos_obsequiados = 1  
+
+print(f"Sueldo bruto: S/. {sueldo_bruto}")
+print(f"Descuento: S/. {descuento}")
+print(f"Sueldo neto: S/. {sueldo_neto}")
+print(f"Número de polos obsequiados: {polos_obsequiados}")
