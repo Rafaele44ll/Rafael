488aae1e1c4571ae97bf2acaaf642f2f1f8e1f61
Rafael <davidrafaelezxc@gmail.com>
2024-10-13 18:39:01 -0500

    py

--- secuenciales/12.py
+++ secuenciales/12.py
@@ -1,2 +1,14 @@
-import math
+import os
+os.system("cls")
 
+a = int(input("Ingrese el coeficiente a (entero): "))
+b = int(input("Ingrese el coeficiente b (entero): "))
+c = int(input("Ingrese el coeficiente c (entero): "))
+
+discriminante = b**2 - 4*a*c
+
+raiz1 = (-b + int(discriminante**0.5)) // (2 * a)
+raiz2 = (-b - int(discriminante**0.5)) // (2 * a)
+
+print("Raíz 1:", raiz1)
+print("Raíz 2:", raiz2)
