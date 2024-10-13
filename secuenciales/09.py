cd18b7cb2da1163acf3ba1ec39ec2e6e70521bc6
Rafael <davidrafaelezxc@gmail.com>
2024-10-13 18:23:42 -0500

    py

--- secuenciales/09.py
+++ secuenciales/09.py
@@ -1,3 +1,13 @@
 import os
 os.system("cls")
 
+numero = int(input("Ingrese un número entero positivo de 4 cifras: "))
+
+suma_cifras = 0
+
+suma_cifras += (numero // 1000)        
+suma_cifras += (numero // 100 % 10)      
+suma_cifras += (numero // 10 % 10)       
+suma_cifras += (numero % 10)             
+
+print(f"La suma de las cifras es: {suma_cifras}")
