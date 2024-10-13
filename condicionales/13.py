a6602acbb5b25a6dd3c2e897b3e65af770437f33
Rafael <davidrafaelezxc@gmail.com>
2024-10-13 10:58:57 -0500

    py

--- condicionales/13.py
+++ condicionales/13.py
@@ -1,2 +1,13 @@
 import os
 os.system("cls")
+
+numero = int(input("Ingrese un número de tres cifras: "))
+
+centenas = numero // 100
+decenas = (numero // 10) % 10
+unidades = numero % 10
+
+if (centenas + 1 == decenas and decenas + 1 == unidades) or (centenas - 1 == decenas and decenas - 1 == unidades):
+    print("Las cifras son consecutivas.")
+else:
+    print("Las cifras no son consecutivas.")
