76a53c9b3bca0cb942bdef22a7eb9950fa2ce5eb
Rafael <davidrafaelezxc@gmail.com>
2024-10-13 17:35:21 -0500

    py

--- condicionales/09.py
+++ condicionales/09.py
@@ -1,2 +1,38 @@
 import os
 os.system("cls")
+
+codigo = int ( input ( "codigo del producto: " ) )
+unidades = int ( input ( "unidades: " ) )
+
+precioUnitario = 0
+descuento = 0
+
+if codigo == 101:
+    precioUnitario = 17
+    if 1 <= unidades <= 10:
+        descuento = 0.05
+elif codigo == 102:
+        precioUnitario = 25
+        if 11 <= unidades <= 20:
+            descuento = 0.18
+elif codigo == 103:
+        precioUnitario = 16
+        if 21 <= unidades <= 30:
+            descuento = 0.10
+elif codigo == 104:
+        precioUnitario = 27
+        if unidades >= 31:
+            descuento = 0.13
+
+compra = precioUnitario * unidades
+descuentoAplicado = compra * descuento
+total = compra - descuentoAplicado
+
+print ( f" Importe de la compra: S/. {compra:.2f}" )
+print ( f" Descuento: S/. {descuentoAplicado:.2f}" )
+print ( f" Total a pagar: S/. {total:.2f}" )
+
+     
+
+
+   
