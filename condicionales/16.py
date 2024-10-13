f3fc4160a878250e1ffd91a6ae4489cd0f1ffb5c
Rafael <davidrafaelezxc@gmail.com>
2024-10-13 11:27:00 -0500

    py

--- condicionales/16.py
+++ condicionales/16.py
@@ -1,2 +1,16 @@
 import os
 os.system("cls")
+
+ingreso_mensual = int(input("Ingrese el ingreso mensual del comprador (en S/.): "))
+
+costo_casa = int(input("Ingrese el costo de la casa (en S/.): "))
+
+if ingreso_mensual < 1250:
+    cuota_inicial = (15 * costo_casa) // 100  
+    monto_cuota_mensual = (costo_casa - cuota_inicial) // 120  
+else:
+    cuota_inicial = (30 * costo_casa) // 100  
+    monto_cuota_mensual = (costo_casa - cuota_inicial) // 75  
+
+print(f"Cuota inicial: S/. {cuota_inicial}")
+print(f"Monto de la cuota mensual: S/. {monto_cuota_mensual}")
