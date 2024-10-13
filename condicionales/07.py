ac75ba6ae6a2804c72b76c707c1d94011821cc29
Rafael <davidrafaelezxc@gmail.com>
2024-10-12 21:49:54 -0500

    py

--- condicionales/07.py
+++ condicionales/07.py
@@ -1,2 +1,15 @@
 import os
 os.system("cls")
+
+num1 = int(input("Ingrese el primer número: "))
+num2 = int(input("Ingrese el segundo número: "))
+num3 = int(input("Ingrese el tercer número: "))
+
+if (num1 > num2 and num1 < num3) or (num1 < num2 and num1 > num3):
+    numero_intermedio = num1
+elif (num2 > num1 and num2 < num3) or (num2 < num1 and num2 > num3):
+    numero_intermedio = num2
+else:
+    numero_intermedio = num3
+
+print(f"El número intermedio es: {numero_intermedio}")
