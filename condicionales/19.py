d457c2e5d82853c82b21bdef1a550afa8fd0c63c
Rafael <davidrafaelezxc@gmail.com>
2024-10-13 12:07:50 -0500

    py

--- condicionales/19.py
+++ condicionales/19.py
@@ -1,2 +1,23 @@
 import os
 os.system("cls")
+
+sexo = input("Ingrese el sexo del postulante (F/M): ").upper()
+
+edad = int(input("Ingrese la edad del postulante: "))
+
+categoria = ""
+
+if sexo == "F":
+    if edad < 23:
+        categoria = "FA"  
+    else:
+        categoria = "FB"  
+elif sexo == "M":
+    if edad < 25:
+        categoria = "MA"  
+    else:
+        categoria = "MB"  
+else:
+    categoria = "Sexo no válido"
+
+print(f"La categoría del postulante es: {categoria}")
