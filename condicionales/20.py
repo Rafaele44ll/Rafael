349783dc2091d71470b41f777fcd6328a0217ec3
Rafael <davidrafaelezxc@gmail.com>
2024-10-13 12:24:19 -0500

    py

--- condicionales/20.py
+++ condicionales/20.py
@@ -1,2 +1,15 @@
 import os
 os.system("cls")
+
+a = int(input("Ingrese el primer número (a): "))
+b = int(input("Ingrese el segundo número (b): "))
+c = int(input("Ingrese el tercer número (c): "))
+
+if a < b < c:
+    orden = "ascendente"
+elif a > b > c:
+    orden = "descendente"
+else:
+    orden = "desordenado"
+
+print(f"Los números fueron ingresados en orden {orden}.")
