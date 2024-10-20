14e1e2d1bd02d20e5f5ad60bc9bb027eba57e5c3
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-20 12:20:17 -0500

    py

--- Repetitivas/04.py
+++ Repetitivas/04.py
@@ -1,2 +1,13 @@
 import os
 os.system("cls")
+
+def mostrar_multiplos(n, m, contador=1):
+    if contador > m:
+        return
+    else:
+        print(n * contador)
+        mostrar_multiplos(n, m, contador + 1)
+
+n = int(input("Ingrese un número n: "))
+m = int(input("Ingrese cuántos múltiplos desea mostrar (m): "))
+mostrar_multiplos(n, m)
