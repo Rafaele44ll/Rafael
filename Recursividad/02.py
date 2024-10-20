53c81b5d9ace6a8066a4220eae5e4c6bcafaec69
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-20 01:01:32 -0500

    py

--- Recursividad/02.py
+++ Recursividad/02.py
@@ -1,2 +1,13 @@
 import os
 os.system("cls")
+
+import os
+os.system("cls")
+
+def mcd(a, b):
+    if b == 0:
+        return a
+    return mcd(b, a % b)
+
+resultado = mcd(48, 18)
+print(resultado)  
