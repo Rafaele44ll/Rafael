8775636753a465e2fa1a09254f5a46ec9bbd3c9c
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-20 00:45:34 -0500

    py

--- Recursividad/01.py
+++ Recursividad/01.py
@@ -1,2 +1,15 @@
 import os
 os.system("cls")
+
+import os
+os.system("cls")
+
+def invertir_cadena(cadena):
+    if len(cadena) <= 1:
+        return cadena
+    
+    return cadena[-1] + invertir_cadena(cadena[:-1])
+
+texto = "hola"
+resultado = invertir_cadena(texto)
+print(resultado)  
