e766934a76f1128bf21170074a36cd1a270e0e32
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-20 02:42:41 -0500

    py

--- Repetitivas/03.py
+++ Repetitivas/03.py
@@ -1,2 +1,14 @@
 import os
 os.system("cls")
+
+def contar_divisores(n, divisor=1):
+    if divisor > n:
+        return 0
+    if n % divisor == 0:
+        return 1 + contar_divisores(n, divisor + 1)
+    else:
+        return contar_divisores(n, divisor + 1)
+
+numero = 12
+cantidad_divisores = contar_divisores(numero)
+print(f"La cantidad de divisores de {numero} es {cantidad_divisores}")
