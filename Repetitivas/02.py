6c45c28d4b468023f3c7cf785495808129316bae
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-20 13:49:29 -0500

    py

--- Repetitivas/02.py
+++ Repetitivas/02.py
@@ -2,14 +2,11 @@ import os
 os.system("cls")
 
 def multiplicar(a, b):
-    # Caso base: si uno de los números es 0
     if b == 0:
         return 0
-    # Si b es negativo, llamamos a la función con -b
     elif b < 0:
         return -multiplicar(a, -b)
     else:
-        # Suma a con el resultado de multiplicar a por b-1
         return a + multiplicar(a, b - 1)
 
 num1 = 5
--- Repetitivas/07.py
+++ Repetitivas/07.py
@@ -1,2 +1,15 @@
 import os
 os.system("cls")
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+numero = int(input("Introduce un número para calcular su factorial: "))
+if numero < 0:
+    print("El factorial no está definido para números negativos.")
+else:
+    resultado = factorial(numero)
+    print(f"El factorial de {numero} es {resultado}.")
