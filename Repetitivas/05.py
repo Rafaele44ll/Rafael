8dc6250da449df74217a6e132efa9d06cf66cfc8
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-20 12:40:35 -0500

    py

--- Repetitivas/05.py
+++ Repetitivas/05.py
@@ -1,2 +1,13 @@
 import os
 os.system("cls")
+
+def tabla_multiplicar(n, i=1):
+    if i > 12:
+        return
+    else:
+        print(f"{n} x {i} = {n * i}")
+        tabla_multiplicar(n, i + 1)
+
+n = int(input("Ingrese un número n para generar su tabla de multiplicar: "))
+print(f"Tabla de multiplicar del {n}:")
+tabla_multiplicar(n)
