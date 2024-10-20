5e966035fe85d61e5cfe1acd9efdb15695de84ad
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-20 18:20:26 -0500

    py

--- Repetitivas/15.py
+++ Repetitivas/15.py
@@ -1,2 +1,21 @@
 import os
 os.system("cls")
+
+def a_mayusculas(cadena):
+    if not cadena:
+        return ""
+    
+    return cadena[0].upper() + a_mayusculas(cadena[1:])
+
+def a_minusculas(cadena):
+    if not cadena:
+        return ""
+    
+    return cadena[0].lower() + a_minusculas(cadena[1:])
+texto = input("Introduce una cadena de texto: ")
+
+texto_mayusculas = a_mayusculas(texto)
+texto_minusculas = a_minusculas(texto)
+
+print(f"Texto en mayúsculas: {texto_mayusculas}")
+print(f"Texto en minúsculas: {texto_minusculas}")
