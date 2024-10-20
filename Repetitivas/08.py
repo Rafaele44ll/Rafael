678586614f242403a15627cbaa1e4f11119c9096
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-20 14:31:02 -0500

    py

--- Repetitivas/08.py
+++ Repetitivas/08.py
@@ -1,2 +1,14 @@
 import os
 os.system("cls")
+
+def potencia(n, m):
+    if m == 0:
+        return 1
+    else:
+        return n * potencia(n, m - 1)
+
+base = float(input("Introduce la base (n): "))
+exponente = int(input("Introduce el exponente (m): "))
+
+resultado = potencia(base, exponente)
+print(f"{base} elevado a la {exponente} es {resultado}.")
