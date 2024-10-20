3b7dd0a53ab2e2aa1dc4422cb265a6cf879fdb88
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-20 18:11:32 -0500

    py

--- Repetitivas/14.py
+++ Repetitivas/14.py
@@ -1,2 +1,19 @@
 import os
 os.system("cls")
+
+def es_primo(n, divisor=None):
+    if n < 2:
+        return False
+    if divisor is None:
+        divisor = n - 1
+    if divisor == 1:
+        return True
+    if n % divisor == 0:
+        return False
+    return es_primo(n, divisor - 1)
+
+numero = int(input("Introduce un número entero: "))
+if es_primo(numero):
+    print(f"{numero} es un número primo.")
+else:
+    print(f"{numero} no es un número primo.")
