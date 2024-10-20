c393ad86b90c8cc15963f45a6ebe4b11d606a07e
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-20 13:15:59 -0500

    py

--- Repetitivas/06.py
+++ Repetitivas/06.py
@@ -1,2 +1,18 @@
 import os
 os.system("cls")
+
+def tabla_multiplicar(n, x, y, i=None):
+    if i is None:
+        i = x  
+
+    if i > y:
+        return
+    else:
+        print(f"{n} x {i} = {n * i}")
+        tabla_multiplicar(n, x, y, i + 1)
+
+n = int(input("Ingrese un número n para generar su tabla de multiplicar: "))
+x = int(input("Ingrese el valor mínimo x: "))
+y = int(input("Ingrese el valor máximo y: "))
+print(f"Tabla de multiplicar del {n} desde {x} hasta {y}:")
+tabla_multiplicar(n, x, y)
