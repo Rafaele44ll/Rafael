dfda7a8dd10b626d8c9597d2906fb47ca94d8f33
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-20 15:30:05 -0500

    py

--- Repetitivas/09.py
+++ Repetitivas/09.py
@@ -1,2 +1,15 @@
 import os
 os.system("cls")
+
+def dibujar_rectangulo(n, altura_actual=0):
+    if altura_actual == n:
+        return
+    print('*' * (2 * n))
+    dibujar_rectangulo(n, altura_actual + 1)
+
+n = int(input("Introduce la altura del rectángulo (n >= 4): "))
+
+if n >= 4:
+    dibujar_rectangulo(n)
+else:
+    print("La altura debe ser al menos 4.")
