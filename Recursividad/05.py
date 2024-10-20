cf2234a318ce91d95fe76f9bec644d4dd74587fc
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-20 01:38:24 -0500

    py

--- Recursividad/05.py
+++ Recursividad/05.py
@@ -1,2 +1,16 @@
 import os
 os.system("cls")
+
+def permutaciones(cadena):
+    if len(cadena) == 1:
+        return [cadena]
+    
+    resultado = []
+    for i, letra in enumerate(cadena):
+        subcadena = cadena[:i] + cadena[i+1:]
+        for perm in permutaciones(subcadena):
+            resultado.append(letra + perm)
+    
+    return resultado
+
+print(permutaciones("abc")) 
