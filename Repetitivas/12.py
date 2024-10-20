984f3fb2e1e4fdbd3d0d3637aa02dff7b8a9ca83
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-20 17:08:47 -0500

    py

--- Repetitivas/12.py
+++ Repetitivas/12.py
@@ -1,2 +1,14 @@
 import os
 os.system("cls")
+
+def mostrar_numeros(n, limite=100):
+    if n > limite:
+        return
+    print(n, end=' ')
+    
+    if n % 10 == 0:
+        print()  
+
+    mostrar_numeros(n + 1, limite)
+
+mostrar_numeros(1)
