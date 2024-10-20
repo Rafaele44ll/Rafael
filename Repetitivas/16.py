6e022b5ba794637ae92f80ad15cab9f166ec7ff4
Rafaele44ll <davidrafaelezxc@gmail.com>
2024-10-20 18:51:30 -0500

    py

--- Repetitivas/16.py
+++ Repetitivas/16.py
@@ -1,2 +1,13 @@
 import os
 os.system("cls")
+
+def invertir_cadena(cadena):
+    if len(cadena) <= 1:
+        return cadena
+    
+    return cadena[-1] + invertir_cadena(cadena[:-1])
+
+texto = input("Introduce una cadena de texto: ")
+cadena_invertida = invertir_cadena(texto)
+
+print(f"Cadena invertida: {cadena_invertida}")
